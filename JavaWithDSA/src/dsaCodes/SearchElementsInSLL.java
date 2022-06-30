c9710b5863a1f64d828d5db9c178945c7f79b3a1
Sagar <you@example.com>
2022-06-30 18:11:09 +0530

    Code Added for getting nth node from end of linked list

--- JavaWithDSA/src/dsaCodes/SearchElementsInSLL.java
+++ JavaWithDSA/src/dsaCodes/SearchElementsInSLL.java
@@ -1,5 +1,7 @@
 package dsaCodes;
 
+import java.util.Scanner;
+
 public class SearchElementsInSLL extends ListNodeClass {
 	
 	
@@ -34,6 +36,37 @@ public class SearchElementsInSLL extends ListNodeClass {
 		}
 		return slowPtr;
 	}
+	
+	//find nth node from the end of a linked list
+	public ListNode getNthNodeFromEnd(int n)
+	{
+		if(head==null)
+		{
+			return null;
+		}
+		if(n<=0)
+		{
+			throw new IllegalArgumentException("Invalid value: n = "+n);
+		}
+		ListNode mainPtr = head;
+		ListNode refPtr = head;
+		int count=0;
+		while(count<n)
+		{
+			if(refPtr==null)
+			{
+				throw new IllegalArgumentException(n+" is greater than the number of nodes in the list.");
+			}
+			refPtr =refPtr.next;
+			count++;
+		}
+		while(refPtr!=null)
+		{
+			mainPtr=mainPtr.next;
+			refPtr =refPtr.next;
+		}
+		return mainPtr;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -42,11 +75,18 @@ public class SearchElementsInSLL extends ListNodeClass {
 		se.insert(2);
 		se.insert(7);
 		se.insert(4);
+		se.insert(8);
 		System.out.println("Singly Linked List:");
 		se.display();
 		System.out.println("Is the search key found? : "+se.searchElement(5));
 		ListNode middleNode = se.getMiddleNode();
 		System.out.println("Middle node is :"+middleNode.data);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the node position from end:");
+		int n = sc.nextInt();
+		ListNode node = se.getNthNodeFromEnd(n);
+		System.out.println("Nth node from end is "+node.data);
+		sc.close();
 
 	}
 
