480cd3bb4e0b04705c53391a2cc5af685bdddc9d
Sagar <you@example.com>
2022-07-03 18:02:30 +0530

    Code added for Insert new node in sorted linked list in Sort and remove duplicate code

--- JavaWithDSA/src/dsaCodes/SortAndRemoveDuplicatesFromSLL.java
+++ JavaWithDSA/src/dsaCodes/SortAndRemoveDuplicatesFromSLL.java
@@ -33,6 +33,26 @@ public class SortAndRemoveDuplicatesFromSLL extends ListNodeClass {
 		}
 	}
 	
+	//Insert a new node in sorted linked list that won't effect on sorting order
+	public ListNode insertInSortedList(int value)
+	{
+		ListNode newNode = new ListNode(value);
+		if(head==null)
+		{
+			return newNode;
+		}
+		ListNode current = head;
+		ListNode temp = null;
+		while(current!=null && current.data<newNode.data)
+		{
+			temp = current;
+			current = current.next;
+		}
+		newNode.next = current;
+		temp.next = newNode;
+		return head;
+	}
+	
 	//Remove Duplicates from linked list
 	public void removeDuplicates()
 	{
@@ -73,6 +93,10 @@ public class SortAndRemoveDuplicatesFromSLL extends ListNodeClass {
 		System.out.println("Remove Duplicates from given sorted list:");
 		sr.removeDuplicates();
 		sr.display();
+		System.out.println("Inserted new node in a sorted linked list:");
+		sr.insertInSortedList(5);
+		sr.display();
+		
 
 	}
 
