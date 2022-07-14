bd27616c42f3ca41e773fb1e4816064e8aa8f7b4
Sagar <you@example.com>
2022-07-14 17:15:44 +0530

    Insert node at start of Circular linked list

--- JavaWithDSA/src/dsaCodes/CircularLinkedListImplementation.java
+++ JavaWithDSA/src/dsaCodes/CircularLinkedListImplementation.java
@@ -1,7 +1,7 @@
 //It is similar to singly linked list. Only difference - last node connected to first node
 package dsaCodes;
 
-public class CircularLinkedListImplementation extends ListNodeSLL {
+public class CircularLinkedListImplementation {
 	
 	private ListNode last;
 	private int length;
@@ -32,6 +32,7 @@ public class CircularLinkedListImplementation extends ListNodeSLL {
 		return length==0;
 	}
 	
+	//create circular linked list
 	public void createCircularLinkedList()
 	{
 		ListNode first = new ListNode(1);
@@ -47,11 +48,48 @@ public class CircularLinkedListImplementation extends ListNodeSLL {
 		last = fourth;
 	}
 
+	//print circular linked list
+	public void display()
+	{
+		if(last==null)
+		{
+			return;
+		}
+		ListNode first = last.next;
+		while(first!=last)
+		{
+			System.out.print(first.data+" ");
+			first=first.next;
+		}
+		System.out.println(first.data+" ");
+	}
+	
+	//insert a node at the start of a linked list
+	public void insertFirst(int value)
+	{
+		ListNode newNode = new ListNode(value);
+		if(last==null)
+		{
+			last = newNode;
+		}
+		else
+		{
+			newNode.next = last.next;
+		}
+		last.next = newNode;
+		length++;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CircularLinkedListImplementation cll = new CircularLinkedListImplementation();
 		cll.createCircularLinkedList();
-		
+		System.out.println("Print Circular Linked List:");
+		cll.display();
+		System.out.println("Insert a node at the beginning of circular linked list:");
+		cll.insertFirst(2);
+		cll.insertFirst(6);
+		cll.display();
 
 	}
 
