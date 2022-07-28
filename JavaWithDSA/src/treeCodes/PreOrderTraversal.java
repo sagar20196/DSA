7bb066f8fe53349a8d3952e1f4efd77de1109f26
Sagar <you@example.com>
2022-07-28 18:52:36 +0530

    Implementation of binary tree using pre order traversal operation

---
+++ JavaWithDSA/src/treeCodes/PreOrderTraversal.java
@@ -0,0 +1,39 @@
+package treeCodes;
+
+public class PreOrderTraversal extends BinaryTree {
+
+	//PreOrder Traversal of a binary tree
+	public void preOrder(TreeNode root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		System.out.print(root.data+ " ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+	
+	//creating binary tree
+	public void binaryTree()
+	{
+		TreeNode first = new TreeNode(9);
+		TreeNode second = new TreeNode(2);
+		TreeNode third = new TreeNode(3);
+		TreeNode fourth = new TreeNode(4);
+		
+		root = first;
+		first.left = second;
+		first.right = third;
+		second.left = fourth;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PreOrderTraversal pt = new PreOrderTraversal();
+		pt.binaryTree();
+		pt.preOrder(pt.root);
+
+	}
+
+}
