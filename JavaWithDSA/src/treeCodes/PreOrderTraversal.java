39d1125209ba2ffd6ac2744e18b77f1100524a50
Sagar <you@example.com>
2022-07-31 15:08:27 +0530

    Pre Order Traversal of a binary tree using recursion and iteration

---
+++ JavaWithDSA/src/treeCodes/PreOrderTraversal.java
@@ -0,0 +1,55 @@
+package treeCodes;
+
+import java.util.Stack;
+
+public class PreOrderTraversal extends BinaryTree{
+	
+	//PreOrder Traversal of a binary tree using recursion
+	public void recursivePreOrder(TreeNode root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+			System.out.print(root.data+ " ");
+			recursivePreOrder(root.left);
+			recursivePreOrder(root.right);
+	}
+	
+	//PreOrder Traversal of a binary tree using iteration 
+	public void iterativePreOrder(TreeNode root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+		while(!stack.isEmpty())
+		{
+			TreeNode temp = stack.pop();
+			System.out.print(temp.data+" ");
+			if(temp.right!=null)
+			{
+				stack.push(temp.right);
+			}
+			if(temp.left!=null)
+			{
+				stack.push(temp.left);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PreOrderTraversal it = new PreOrderTraversal();
+		it.createBinaryTree();
+		System.out.println("Pre Order Traversal using recursion:");
+		it.recursivePreOrder(it.root);
+		System.out.println();
+		System.out.println("Pre Order Traversal using Iteration:");
+		it.iterativePreOrder(it.root);
+
+	}
+
+}
