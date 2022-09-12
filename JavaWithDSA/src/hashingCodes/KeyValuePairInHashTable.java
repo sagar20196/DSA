919b61a85de30faef5ea3c2bc0d7ff973bc0517b
Sagar <you@example.com>
2022-09-12 16:27:44 +0530

    Get value using key from Hash Table

--- JavaWithDSA/src/hashingCodes/KeyValuePairInHashTable.java
+++ JavaWithDSA/src/hashingCodes/KeyValuePairInHashTable.java
@@ -14,6 +14,7 @@ public class KeyValuePairInHashTable extends HashTable {
 		super(capacity);
 	}
 	
+	//Put key-value pair in Hash Table
 	public void put(Integer key, String value)
 	{
 		if(key==null || value==null)
@@ -45,6 +46,27 @@ public class KeyValuePairInHashTable extends HashTable {
 	{
 		return key % numOfBuckets;
 	}
+	
+	//get value using key from Hash Table
+	public String get(Integer key)
+	{
+		if(key==null)
+		{
+			throw new IllegalArgumentException("Key is null !!");
+		}
+		
+		int bucketIndex = getBucketIndex(key);
+		HashNode head = buckets[bucketIndex];
+		while(head!=null)
+		{
+			if(head.key.equals(key))
+			{
+				return head.value;
+			}
+			head = head.next;
+		}
+		return null;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -54,6 +76,10 @@ public class KeyValuePairInHashTable extends HashTable {
 		hs.put(21, "Harry");
 		hs.put(31, "Mary");
 		System.out.println("Number of Key-Value pair in Hash Table:\n"+hs.size);
+		String s1 = hs.get(21);
+		String s2 = hs.get(45);
+		String s3 = hs.get(105);
+		System.out.println("Get value using Key from Hash Table:\n"+ s1+"\n"+s2+"\n"+s3);
 
 	}
 
