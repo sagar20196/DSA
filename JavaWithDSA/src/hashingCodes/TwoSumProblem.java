2001350a49e621a21f0d7b6ab50b0eb44e30f403
Sagar <you@example.com>
2022-09-15 17:06:11 +0530

    Return numbers whose sum is equal to given target using sorting and two pointer technique

--- JavaWithDSA/src/hashingCodes/TwoSumProblem.java
+++ JavaWithDSA/src/hashingCodes/TwoSumProblem.java
@@ -13,8 +13,9 @@ public class TwoSumProblem extends HashTable{
 		super(capacity);
 	}
 	
+	//two sum problem using HashMap
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public int[] twoSum(int [] arr, int target)
+	public int[] twoSum1(int [] arr, int target)
 	{
 		int[] result = new int[2];
 		Map map = new HashMap();
@@ -33,14 +34,45 @@ public class TwoSumProblem extends HashTable{
 		}
 		return result;
 	}
+	
+	//two sum problem using sorting and two pointer technique
+	public int[] twoSum2(int[] arr, int target)
+	{
+		Arrays.sort(arr);
+		int left = 0;
+		int right = arr.length - 1;
+		int[] result = new int[2];
+		while(left<right)
+		{
+			int sum = arr[left] + arr[right];
+			if(sum==target)
+			{
+				result[0] = arr[left];
+				result[1] = arr[right];
+				return result;
+			}
+			else if(sum<target)
+			{
+				left++;
+			}
+			else
+			{
+				right--;
+			}
+		}
+		return result;
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TwoSumProblem ts = new TwoSumProblem(6);
 		int [] arr = {2,11,5,10,7,8};
-		int[] result = ts.twoSum(arr, 9);
-		System.out.println("Return index of two elements whose sum is equal to given target:\n"+Arrays.toString(result));
-
+		int[] result1 = ts.twoSum1(arr, 9);
+		int[] result2 = ts.twoSum2(arr, 15);
+		System.out.println("Return index of two elements whose sum is equal to given target:\n"+Arrays.toString(result1));
+		System.out.println("Return numbers whose sum is equal to given target using sorting and two pointer:\n"+Arrays.toString(result2));
+		
 	}
 
 }
